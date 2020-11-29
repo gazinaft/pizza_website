813080f1ded32ca73f8975b0196c810f3b4b705f
gazinaft <gazinaft@gmail.com>
2020-11-29 20:06:08 +0200

    https

--- client.js
+++ client.js
@@ -1,12 +1,12 @@
 export default class Client {
   getData(endpoint) {
-    return fetch(`http://my-json-server.typicode.com/gazinaft/pizza_db/${endpoint}`)
+    return fetch(`https://my-json-server.typicode.com/gazinaft/pizza_db/${endpoint}`)
         .then(response => response.json())
         .catch(e => console.log(e));
   }
 
   post(data, endpoint) {
-    return fetch(`http://my-json-server.typicode.com/gazinaft/pizza_db/${endpoint}`, { 
+    return fetch(`https://my-json-server.typicode.com/gazinaft/pizza_db/${endpoint}`, { 
       method: "POST",  
       body: JSON.stringify(data)
     })
